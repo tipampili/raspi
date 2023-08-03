b9ac741d130280f6eefded5e669f399343d5874b
tipampili <128613317+tipampili@users.noreply.github.com>
2023-08-03 15:37:07 -0300

    Update ponto.py

--- ponto.py
+++ ponto.py
@@ -16,13 +16,15 @@ class PontoGUI(tk.Tk):
         self.time_label = tk.Label(self, font=("Arial", 35))
         self.time_label.pack()
         self.update_time()
-        self.status_label = tk.Label(self, text="Aproxime o Chachá...", font=("Arial", 30), fg="#F400A1", anchor="w", wraplength=450)
+        self.status_label = tk.Label(self, text="Aproxime o Crachá...", font=("Arial", 30), fg="#F400A1", anchor="w", wraplength=450)
         self.status_label.pack(pady=20)
         self.numero = ""
         self.bind("<Key>", self.key_pressed)
         self.funcionarios = {}
         self.atualizar_dados_apex()
         self.dados = []
+        self.badge_queue = asyncio.Queue()
+        self.data_queue = asyncio.Queue()
 
     def update_time(self):
         current_time = datetime.datetime.now().strftime("%H:%M:%S")
@@ -39,7 +41,7 @@ class PontoGUI(tk.Tk):
             else:
                 print("Número inválido")
                 self.numero = ""
-                self.after(1000, lambda: self.status_label.config(text="Aproxime o Chachá..."))
+                self.after(1000, lambda: self.status_label.config(text="Aproxime o Crachá..."))
 
     def verificar_conexao_internet(self):
         try:
@@ -72,21 +74,20 @@ class PontoGUI(tk.Tk):
             self.status_label.config(text=f"{nome_funcionario}", background="green", fg="#FFFFFF", font=("Arial", 30, "bold"))
             self.configure(background="green")
         else:
-            self.status_label.config(text="Ponto registrado!", background="green", fg="#FFFFFF", font=("Arial", 30, "bold"))
+            self.status_label.config(text="Crachá inexistente, ponto registrado!", background="green", fg="#FFFFFF", font=("Arial", 30, "bold"))
             self.configure(background="green")
 
         self.after(700, lambda: self.configure(background="#F0F0F0"))
-        self.after(700, lambda: self.status_label.config(text="Aproxime o Chachá...", background="#F0F0F0", fg="#F400A1"))
+        self.after(700, lambda: self.status_label.config(text="Aproxime o Crachá...", background="#F0F0F0", fg="#F400A1"))
 
         self.numero = ""
 
     async def enviar_dados_periodicos(self):
         while True:
-            await asyncio.sleep(3600) 
+            await asyncio.sleep(3600)
 
-            if self.verificar_conexao_internet():
+            if self.verificar_conexao_internet() and self.dados:
                 dados_enviar = self.dados.copy()
-                self.dados.clear()
 
                 url = 'https://apex.pampili.com.br/ords/afvserver/ponto/pontoparanaiba'
                 headers = {'Content-type': 'application/json'}
@@ -103,20 +104,32 @@ class PontoGUI(tk.Tk):
 
                     if response.status_code == 200:
                         print(f"Dado {dado.strip()} enviado com sucesso para o sistema no Apex Oracle.")
+                        self.dados.remove(dado)
                     else:
                         print(f"Falha ao enviar dado {dado.strip()}. Status Code: {response.status_code}")
 
-            else:
-                self.dados.extend(dados_enviar) 
+    async def processar_dados_ponto(self):
+        while True:
+            badge_number = await self.badge_queue.get()
+            horario = datetime.datetime.now().strftime(f'%d%m%y%H%M')
+            dado = f"{badge_number};{horario}\n"
+            self.dados.append(dado)
+
+    async def coletar_dados_ponto(self):
+        while True:
+            dado = await self.data_queue.get()
+            self.dados.append(dado)
 
     def iniciar_interface_grafica(self):
         self.mainloop()
 
     def iniciar_loop_async(self):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.create_task(self.enviar_dados_periodicos())
-        loop.run_forever()
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+        self.loop.create_task(self.enviar_dados_periodicos())
+        self.loop.create_task(self.processar_dados_ponto())
+        self.loop.create_task(self.coletar_dados_ponto())
+        self.loop.run_forever()
 
 if __name__ == "__main__":
     app = PontoGUI()
@@ -126,9 +139,8 @@ if __name__ == "__main__":
             with open("/home/pi/raspi/dados.txt", "r") as arquivo:
                 dados = arquivo.readlines()
             if dados:
-                Thread(target=app.enviar_dados, args=(dados,)).start()
-
-        app.after(300000, verificar_conexao_e_enviar_dados)
+                for dado in dados:
+                    app.dados.append(dado)
 
     verificar_conexao_e_enviar_dados()
 
