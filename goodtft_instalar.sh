408f805e9d85be2298c98a5690c88d5bc856b053
tipampili <128613317+tipampili@users.noreply.github.com>
2024-06-07 11:15:58 -0300

    Update goodtft_instalar.sh

--- goodtft_instalar.sh
+++ goodtft_instalar.sh
@@ -1,6 +1,12 @@
 #! /bin/sh
 sudo apt update
 sudo apt upgrade -y
+sudo apt-get install x11vnc
+x11vnc -usepw -forever -display: 0
+sudo cp x11vnc.desktop /home/pi/.config/autostart/x11vnc.desktop
+sudo x11vnc -storepasswd /etc/x11vnc.pass
+sudo cp x11vnc.service /lib/systemd/system/x11vnc.service
+sudo systemctl enable x11vnc.service
 sudo mkdir /home/pi/.config/autostart
 chmod -R 777 /home/pi/.config/autostart
 sudo cp auto.desktop /home/pi/.config/autostart/auto.desktop
